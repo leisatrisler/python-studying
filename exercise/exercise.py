c8d1f89c8c099211517b261a30b3c7107c147359
Leisa Trisler <trisler.leisa@gmail.com>
2023-04-19 13:45:50 -0700

    changes

---
+++ exercise/exercise.py
@@ -0,0 +1,13 @@
+# We check in a patient named John Smith
+# He's 20 yrs old
+# He is a new patient
+# Declare a few variables
+
+age = 20
+first_name = "John"
+last_name = "Smith"
+
+x = first_name + " " + last_name
+if x:
+    print("20")
+    print(x)
