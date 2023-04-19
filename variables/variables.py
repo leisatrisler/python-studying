bd58f9b716808a2fa90b2f8a7e7cc9994b655648
Leisa Trisler <trisler.leisa@gmail.com>
2023-04-19 13:33:15 -0700

    changes

---
+++ variables/variables.py
@@ -0,0 +1,5 @@
+age = 20
+price = 19.95  # python always usage of numbers with a decimal point or whole numbers
+first_name = "Mosh"
+is_online = True  # true or false is called a boolan value # true or false most be True or False in python to reflect the boolan value # that is how we declare and use variables in python
+print(age)  # no quotes inside otherwise it will print the word "age" rather than the variable
