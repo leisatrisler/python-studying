265beea509fc967d1651875f4a29c50d8dff6c33
Leisa Trisler <trisler.leisa@gmail.com>
2023-04-19 20:22:17 -0700

    changes

---
+++ aritmetic_opertors/aritmetic_operators.py
@@ -0,0 +1,13 @@
+# Aritmetic operators in Python are the same we have in Math
+# We can add numbers, subtract them, multiple, etc
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)  # Technically we have 2 different types of divison operators (/ gives us a floating number with a decimal point)
+# Technically we have 2 different types of divison operators (// gives us an integer, whole number)
+print(10 // 3)
+print(10 ** 3)  # This is 10 to the power of 3
+
+x = 10
+x = x + 3
+x += 3
