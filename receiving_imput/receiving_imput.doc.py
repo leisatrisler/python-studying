50b9456bfcb90a18d218681b68d80716c707ff3a
Leisa Trisler <trisler.leisa@gmail.com>
2023-04-19 19:19:42 -0700

    changes

--- receiving_imput/receiving_imput.doc.py
+++ receiving_imput/receiving_imput.doc.py
@@ -1,3 +1,5 @@
 # how to receive imput from the user
 # python has another built in fuction called "imput"
-input("What is your name ")
+name = input("What is your name? ")
+# this is string concatenation, combining one string with another string
+print("Hello " + name)  # this is how you run the imput function in python
