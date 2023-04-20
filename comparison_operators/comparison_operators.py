93acb5b6a63e1d85e2d986c0b7479cdf68bf7606
Leisa Trisler <trisler.leisa@gmail.com>
2023-04-19 20:42:30 -0700

    changes

---
+++ comparison_operators/comparison_operators.py
@@ -0,0 +1,21 @@
+# In Python there are aritmetic operators, but there is another set called comparison operators
+# Use this operators to compare values
+x = 3 > 2
+print(x)  # When ran we get a Boolean answer
+
+x = 3 >= 2
+print(x)
+x = 3 < 2
+print(x)
+x = 3 <= 2
+print(x)
+x = 3 == 2  # Equality operator, do not confuse this with assignment operator # Here we compare 3 & 2 for equality
+print(x)
+x = 3 != 2  # Not equality operator,indicated by !, followed by =
+
+>  # Greater than
+>=  # Greater than or equal to
+<  # Less than
+<=  # Less than or equal to
+==  # Equal to
+!=  # Not equal
