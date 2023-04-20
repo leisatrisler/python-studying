a0c90129ca4a60dc8c6fc18ded7d79d342dce594
Leisa Trisler <trisler.leisa@gmail.com>
2023-04-19 18:57:17 -0700

    changes

---
+++ type_conversion/type_conversion.py
@@ -0,0 +1,6 @@
+# 3 types of data in python - (numbers) 10, (strings) "Mosh", & (Boolean) True
+# There are times you want to convert the value of a varable from one type to another
+birth_year = input("Enter your birth year: ")  # 1st line input function
+# 2nd line we passed a string to our end function, and will return the end function of the birth year, then we subtract it from 2020
+age = 2020 - int(birth_year)
+print(age)  # 3rd line we get the age and store it in the age variable
